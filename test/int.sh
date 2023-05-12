7138f86f49fa07ade61f1b851a063bb020193ab9
Juan Manuel MARTINEZ CAAMAÑO <jmartinezcaamao@gmail.com>
2023-05-12 18:22:14 +0200

    Remove old test

--- test/int.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eou pipefail
-
-ARG_VARS=$( python3 ../bash-argparse.py \
-              --program "${BASH_SOURCE[0]}" \
-              --signature "int count" \
-              -- "$@" )
-eval ${ARG_VARS}
-echo "count is:" ${COUNT}
