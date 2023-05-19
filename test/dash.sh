3507ad9ce41d8aab54f16d921e741b24691b9aaf
Juan Manuel MARTINEZ CAAMAÑO <jmartinezcaamao@gmail.com>
2023-05-19 16:50:19 +0200

    migrate dash.sh test

--- test/dash.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eou pipefail
-
-ARG_VARS=$( python3 ../bash-argparse.py \
-              --program "${BASH_SOURCE[0]}" \
-              --signature "int dash_count" \
-              -- "$@" )
-eval ${ARG_VARS}
-echo "count is:" ${DASH_COUNT}
