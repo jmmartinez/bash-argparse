0b9a2c22db7a66ce185142f7980d1a1444301de0
Juan Manuel MARTINEZ CAAMAÑO <jmartinezcaamao@gmail.com>
2023-05-30 19:23:24 +0200

    Migrate another test

--- run_test.sh
+++ run_test.sh
@@ -12,15 +12,6 @@ bash input_path.sh --this-path=. | grep "the path is:" | grep test
 bash input_path.sh --this-path=$PWD | grep "the path is:" | grep test
 ! bash input_path.sh --this-path=${PWD}_fail
 
-bash positional.sh a b | grep "positional: a b"
-bash positional.sh a b | grep "optional: ahoy"
-bash positional.sh a b --optional c | grep "positional: a b"
-bash positional.sh a b --optional c | grep "optional: c"
-bash positional.sh a --optional c b | grep "positional: a b"
-bash positional.sh a --optional c b | grep "optional: c"
-! bash positional.sh
-! bash positional.sh a
-
 bash output_path.sh | grep "the path is:" | grep test
 bash output_path.sh --this-path=$PWD/output.txt | grep "the path is:" | grep test
 ! bash output_path.sh --this-path=$PWD
