041138dc793db72665b499c57e4fa91ef8edd43a
Juan Manuel MARTINEZ CAAMAÑO <jmartinezcaamao@gmail.com>
2023-08-09 11:46:41 +0200

    missing test

---
+++ test/deduce_program.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# RUN: export PYTHON_EXEC="%{python}"
+# RUN: export BASH_ARGPARSE_PY="%{bash-argparse-script}"
+#
+# RUN: bash %s | %{FileCheck} %s
+# RUN: bash -x %s | %{FileCheck} %s
+# RUN: bash %s toto.sh | %{FileCheck} %s
+# RUN: /bin/bash %s | %{FileCheck} %s
+# RUN: %s | %{FileCheck} %s
+
+set -e
+
+# do an || true to ensure the script doesn't stop after printing the help message
+${PYTHON_EXEC} ${BASH_ARGPARSE_PY} --signature "int option" -- --help 2>&1 \
+    || true
+
+# The deduced name of the program is the name of the script
+# CHECK: usage: deduce_program.sh
